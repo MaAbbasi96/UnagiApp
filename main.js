555f4e40f09c78abe39b4f6f07defbe9a3d1c6bd
ali nemati chari <alinemati@live.com>
2017-07-25 18:48:23 +0430

    network requests

--- main.js
+++ main.js
@@ -3,16 +3,16 @@
  * https://github.com/facebook/react-native
  * @flow
  */
-import SendPostPage from './SendPostPage';
-import React, { Component } from 'react';
-import { StackNavigator, } from 'react-navigation'; 
-import PostItem from './PostItem';
-import Icon from 'react-native-vector-icons/Ionicons';
-import ActionButton from 'react-native-action-button';
-import PTRView from 'react-native-pull-to-refresh';
+import SendPostPage from "./SendPostPage";
+import React, { Component } from "react";
+import { StackNavigator } from "react-navigation";
+import PostItem from "./PostItem";
+import Icon from "react-native-vector-icons/Ionicons";
+import ActionButton from "react-native-action-button";
+import PTRView from "react-native-pull-to-refresh";
 import { getPosts } from "./network";
 //import RCTRefreshControl from 'react-refresh-control';
-var DeviceInfo = require('react-native-device-info');
+var DeviceInfo = require("react-native-device-info");
 
 import {
   AppRegistry,
@@ -25,122 +25,141 @@ import {
   TouchableOpacity,
   Image,
   RefreshControl
-} from 'react-native';
+} from "react-native";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
-    backgroundColor: '#DCEDC8',
+    flexDirection: "column",
+    backgroundColor: "#DCEDC8"
   },
   top: {
-    flexDirection : 'row',
-    backgroundColor: '#8BC34A',
-    height: 80,
+    flexDirection: "row",
+    backgroundColor: "#8BC34A",
+    height: 80
   },
-   fon : {
-     marginLeft : 10,
-     marginTop : 10,
-     fontSize: 26 ,
-     fontWeight: 'bold',
-     color: 'white',
-     fontFamily: 'monospace', 
-   },
+  fon: {
+    marginLeft: 10,
+    marginTop: 10,
+    fontSize: 26,
+    fontWeight: "bold",
+    color: "white",
+    fontFamily: "monospace"
+  },
 
-  sendimage :{
-    height : 35,
-    width : 35,
-    marginRight : 30,
+  sendimage: {
+    height: 35,
+    width: 35,
+    marginRight: 30
     // position : 'absolute',
     // left : 0,
     // top: 0,
-  },
+  }
 });
 
 export default class RahnemaTeam2App extends Component {
   constructor() {
-    super()
-    this.state = {items : [{id:1, label:'ali'}] };
+    super();
     this.getUniqueID.bind(this);
+    this.state = { items: [] };
   }
   static navigationOptions = {
-    title: 'اوناگی',
+    title: "اوناگی",
     headerStyle: {
-      backgroundColor: '#8BC34A'
+      backgroundColor: "#8BC34A"
     },
     headerTitleStyle: {
-      color: '#fff'
+      color: "#fff"
     }
   };
-    getLocation(){
-    return new Promise(function(resolve,reject){
-      navigator.geolocation.getCurrentPosition(
-      (position,err) => {
-        if (err){
-          return reject();
-        }
-        var initialPosition = position;
-        var location;
-        location = {"latitude": initialPosition.coords.latitude, "longitude": initialPosition.coords.longitude};
-        // console.log("Created Location ", location);
-        this.setState({location});
-        // console.log("location",this.state.initialPosition.coords);
-        resolve();
-      },
-      (error) => alert(error.message),
-      {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+
+  getLocation() {
+    return new Promise(
+      function(resolve, reject) {
+        navigator.geolocation.getCurrentPosition(
+          (position, err) => {
+            if (err) {
+              return reject();
+            }
+            var initialPosition = position;
+            var location;
+            location = {
+              latitude: initialPosition.coords.latitude,
+              longitude: initialPosition.coords.longitude
+            };
+            // console.log("Created Location ", location);
+            this.setState({ location });
+            // console.log("location",this.state.initialPosition.coords);
+            resolve();
+          },
+          error => alert(error.message),
+          { enableHighAccuracy: false, timeout: 20000, maximumAge: 1000 }
+        );
+        this.watchID = navigator.geolocation.watchPosition(position => {
+          var lastPosition = JSON.stringify(position);
+          this.setState({ lastPosition });
+        });
+      }.bind(this)
     );
-      this.watchID = navigator.geolocation.watchPosition((position) => {
-      var lastPosition = JSON.stringify(position);
-      this.setState({lastPosition});
-    });
-    }.bind(this))
-    
   }
 
-  getUniqueID(){
-      var id = {"unique_id" : DeviceInfo.getUniqueID()};
-      this.setState(id,()=>{});   
+  getUniqueID(func) {
+    this.setState({ unique_id: DeviceInfo.getUniqueID() }, func);
   }
-  componentWillMount(){
-    this.getUniqueID();
+
+  componentDidMount() {
+    this.getUniqueID(() => {
+      this.getLocation()
+        .then(res =>{
+          console.log('here')
+          return
+          getPosts(this.state.unique_id, {
+            latitude: this.state.location.latitude,
+            longitude: this.state.location.longitude
+          })
+        
+        }
+        )
+        .then(res => this.setState({ items: res }))
+        .catch(err => console.log(err));
+    });
+
+    // getPosts(this.getUniqueID(),{latitude:1,longitude:1}).then((res)=>{this.setState({items : res})});
   }
-  
+
   _refresh() {
-    return new Promise((resolve) => {
-      setTimeout(()=>{resolve()}, 2000)
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve();
+      }, 2000);
     });
   }
-
 
   render() {
-    if (!this.state) {
-      return null
-    }
+    if (!this.state) return null;
     // console.log('our state:', this.state);
     const { navigate } = this.props.navigation;
-    return (  
-    <View style={styles.container}>
-        <PTRView onRefresh={this._refresh} >
+    return (
+      <View style={styles.container}>
+        <PTRView onRefresh={this._refresh}>
           <View>
             <ScrollView>
-              { this.state.items.map(item => (
-                <PostItem
-                  key={item.id}
-                  label={item.label}
-                />  
-                ))} 
-            </ScrollView> 
-          </View> 
+              {this.state.items.map(item =>
+                <PostItem key={item._id} label={item.text} />
+              )}
+            </ScrollView>
+          </View>
         </PTRView>
-        <ActionButton buttonColor="#757575" onPress={() => navigate('SendPostPage')}>
-          </ActionButton>
-       </View> 
+        <ActionButton
+          buttonColor="#757575"
+          onPress={() => navigate("SendPostPage")}
+        />
+      </View>
     );
   }
 }
-const App = StackNavigator({ 
-  Home: { screen: RahnemaTeam2App }, 
-  SendPostPage: { screen: SendPostPage }, 
+const App = StackNavigator({
+  Home: { screen: RahnemaTeam2App },
+  SendPostPage: { screen: SendPostPage }
 });
-AppRegistry.registerComponent('RahnemaTeam2App', () => App);
+AppRegistry.registerComponent("RahnemaTeam2App", () => App);
