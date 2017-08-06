31f69663c6079484e048390550a524ffd055daf4
sh-soltanpour <shahryar.soltanpour@gmail.com>
2017-08-06 14:40:19 +0430

    Show Login page if refreshKey is not found

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
-import { TabNavigator,StackNavigator } from "react-navigation";
+import { TabNavigator, StackNavigator } from "react-navigation";
 import SendPostPage from "./SendPostPage";
 import {
   AppRegistry,
@@ -16,42 +16,56 @@ import {
   Image,
   RefreshControl,
   FlatList,
-  ListView
+  ListView,
+  AsyncStorage
 } from "react-native";
-
+import LoginScreen from "./screens/Login/login";
+import SignupScreen from "./screens/Signup/signup";
 import reducer from "./reducer";
 import NormalPosts from "./NormalPosts";
 import HotPosts from "./HotPosts";
-const MainScreenNavigator = TabNavigator({
-"جدیدترین": { screen: NormalPosts },
-	"داغ‌ترین": { screen: HotPosts },
-},
-	{
-		tabBarOptions: {
-			style: {
-				backgroundColor: '#8BC34A',
-
-			}
-		}
-	});
+const MainScreenNavigator = TabNavigator(
+  {
+    جدیدترین: { screen: NormalPosts },
+    داغ‌ترین: { screen: HotPosts }
+  },
+  {
+    tabBarOptions: {
+      style: {
+        backgroundColor: "#8BC34A"
+      }
+    }
+  }
+);
 MainScreenNavigator.navigationOptions = {
-	title: 'اوناگی',
-	headerStyle: {
-		backgroundColor: '#8BC34A'
-	},
-	headerTitleStyle: {
-		color: '#fff',
-		fontFamily: 'IRAN_Sans'
-	},
+  title: "اوناگی",
+  headerStyle: {
+    backgroundColor: "#8BC34A"
+  },
+  headerTitleStyle: {
+    color: "#fff",
+    fontFamily: "IRAN_Sans"
+  }
 };
-const App = StackNavigator({
-  Home: { screen: MainScreenNavigator },
-  SendPostPage: { screen: SendPostPage }
-});
+
+var auth = true;
+var App = null;
 const setup = () => {
   const store = createStore(reducer, applyMiddleware(thunk));
   class Root extends Component {
+    componentWillMount() {
+      var refreshKey = AsyncStorage.getItem("refreshKey", (error, result) => {
+        if (!result) auth = false;
+         App = StackNavigator({
+          Home: { screen: auth ? MainScreenNavigator : LoginScreen },
+          SignUpPage: { screen: SignupScreen }
+        });
+        this.forceUpdate();
+      });
+    }
     render() {
+      if(!App)
+        return null;
       return (
         <Provider store={store}>
           <App />
