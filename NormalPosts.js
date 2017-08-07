a04293ba0fb7ac557bd2794ba6f4f746d1fdc18d
sh-soltanpour <shahryar.soltanpour@gmail.com>
2017-08-07 18:52:25 +0430

    startup login with tokens fixed

--- NormalPosts.js
+++ NormalPosts.js
@@ -2,28 +2,28 @@ import React, { Component } from "react";
 import Icon from "react-native-vector-icons/Ionicons";
 import ActionButton from "react-native-action-button";
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  TextInput,
-  Button,
-  ScrollView,
-  TouchableOpacity,
-  Image,
-  RefreshControl,
-  FlatList,
-  ListView,
-  AsyncStorage
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    TextInput,
+    Button,
+    ScrollView,
+    TouchableOpacity,
+    Image,
+    RefreshControl,
+    FlatList,
+    ListView,
+    AsyncStorage
 } from "react-native";
 import { connect } from "react-redux";
 import {
-  getAndSaveUniqueID,
-  getAndSavePosts,
-  getAndSaveOldPosts,
-  updatePost,
-  likePost,
-  loginWithToken
+    getAndSaveUniqueID,
+    getAndSavePosts,
+    getAndSaveOldPosts,
+    updatePost,
+    likePost,
+    loginWithToken
 } from "./actions";
 import SendPostPage from "./SendPostPage";
 import PostItem from "./PostItem";
@@ -33,73 +33,72 @@ var Network = require("./network");
 var Helpers = require("./helpers");
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: "column",
-    backgroundColor: "#DCEDC8"
-  }
+    container: {
+        flex: 1,
+        flexDirection: "column",
+        backgroundColor: "#DCEDC8"
+    }
 });
 let myLocation; //= { latitude: 35.7293756, longitude: 51.42246219 };
 
 class NormalPosts extends Component {
-  componentDidMount() {
-    console.log("***********************************************", this.props);
-    if (!this.props.storeState) {
-      AsyncStorage.getItem("refreshToken", (err, result) => {
-        if (result) this.props.loginWithToken(result);
-      });
-    } else {
-      this.props.getAndSavePosts(
-        this.props.storeState.accessToken,
-        this.props.storeState.refreshToken
-      );
+    componentDidMount() {
+        if (!this.props.storeState) {
+            AsyncStorage.getItem("refreshToken", (err, refreshToken) => {
+                if (refreshToken) {
+                    AsyncStorage.getItem("accessToken", (err, accessToken) => {
+                        if (accessToken)
+                            this.props.loginWithToken(
+                                refreshToken,
+                                accessToken
+                            );
+                        this.props.getAndSavePosts(accessToken, refreshToken);
+                    });
+                }
+            });
+        } else {
+            this.props.getAndSavePosts(
+                this.props.storeState.accessToken,
+                this.props.storeState.refreshToken
+            );
+        }
     }
-    // var myLocation = { latitude: 35.7293756, longitude: 51.42246219 };
-    // Helpers.getLocation().then(
-    //   location => {myLocation = location;console.log(location);this.props.getAndSavePosts("ab540b666c9cbce9ab540b666c9cbce9", myLocation);},
-    //   err => console.error(err)
-    // )
-    // this.props.getAndSaveUniqueID();
-
-    // this.props.getAndSavePosts("ab540b666c9cbce9ab540b666c9cbce9");
-    // console.log("PROPSsssssssssssssssss", this.props);
-  }
-  render() {
-    if (!this.props.storeState) return null;
-    const { navigate } = this.props.navigation;
-    return (
-      <View style={styles.container}>
-        <PostsList
-          items={this.props.storeState.items}
-          getAndSavePosts={this.props.getAndSavePosts}
-          getAndSaveOldPosts={this.props.getAndSaveOldPosts}
-          unique_id={this.props.storeState.unique_id}
-          location={this.props.storeState.location}
-          refreshToken={this.props.storeState.refreshToken}
-          accessToken={this.props.storeState.accessToken}
-        />
-        <ActionButton
-          buttonColor="#757575"
-          onPress={() =>
-            navigate("SendPostPage", {
-              accessToken : this.props.storeState.accessToken,
-              refreshToken : this.props.storeState.refreshToken,
-              location: this.props.storeState.location
-            })}
-        />
-      </View>
-    );
-  }
+    render() {
+        if (!this.props.storeState) return null;
+        const { navigate } = this.props.navigation;
+        return (
+            <View style={styles.container}>
+                <PostsList
+                    items={this.props.storeState.items}
+                    getAndSavePosts={this.props.getAndSavePosts}
+                    getAndSaveOldPosts={this.props.getAndSaveOldPosts}
+                    unique_id={this.props.storeState.unique_id}
+                    location={this.props.storeState.location}
+                    refreshToken={this.props.storeState.refreshToken}
+                    accessToken={this.props.storeState.accessToken}
+                />
+                <ActionButton
+                    buttonColor="#757575"
+                    onPress={() =>
+                        navigate("SendPostPage", {
+                            accessToken: this.props.storeState.accessToken,
+                            refreshToken: this.props.storeState.refreshToken,
+                            location: this.props.storeState.location
+                        })}
+                />
+            </View>
+        );
+    }
 }
 mapStateToProps = state => {
-  return {
-    storeState: state
-  };
+    return {
+        storeState: state
+    };
 };
 export default connect(mapStateToProps, {
-  getAndSaveUniqueID,
-  getAndSavePosts,
-  getAndSaveOldPosts,
-  updatePost,
-  loginWithToken
+    getAndSaveUniqueID,
+    getAndSavePosts,
+    getAndSaveOldPosts,
+    updatePost,
+    loginWithToken
 })(NormalPosts);
--- network.js
+++ network.js
@@ -2,224 +2,225 @@ import React, { Component } from "react";
 import Fetch from "react-native-fetch";
 var serverPath = "http://192.168.11.201:3000";
 export function getPosts(location, accessToken, refreshToken) {
-  return fetch(serverPath + "/post", {
-    method: "GET",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-      location: JSON.stringify(location),
-      accesstoken: accessToken,
-      refreshtoken: refreshToken
-    }
-  })
-    .then(function(res) {
-      return res.json();
+    return fetch(serverPath + "/post", {
+        method: "GET",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            location: JSON.stringify(location),
+            accesstoken: accessToken,
+            refreshtoken: refreshToken
+        }
     })
-    .then(function(res) {
-      return new Promise((resolve, reject) => {
-        resolve(res.posts);
-      });
-    })
-    .catch(err => {
-      console.log(err);
-    });
+        .then(function(res) {
+            return res.json();
+        })
+        .then(function(res) {
+            return new Promise((resolve, reject) => {
+                resolve(res.posts);
+            });
+        })
+        .catch(err => {
+            console.log(err);
+        });
 }
 export function login(username, password) {
-  return fetch(serverPath + "/auth/login", {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-      type: "password"
-    },
-    body: JSON.stringify({
-      username: username,
-      password: password
+    return fetch(serverPath + "/auth/login", {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            type: "password"
+        },
+        body: JSON.stringify({
+            username: username,
+            password: password
+        })
     })
-  })
-    .then(function(res) {
-      return res.json();
-    })
-    .then(function(res) {
-      return new Promise((resolve, reject) => {
-        resolve(res);
-      });
-    })
-    .catch(err => {
-      console.log(err);
-    });
+        .then(function(res) {
+            return res.json();
+        })
+        .then(function(res) {
+            return new Promise((resolve, reject) => {
+                resolve(res);
+            });
+        })
+        .catch(err => {
+            console.log(err);
+        });
 }
-export function loginWithToken(refreshToken) {
-  console.log("Refresh token", refreshToken);
-  return fetch(serverPath + "/auth/login", {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-      type: "token",
-      refreshtoken: refreshToken
-    },
-    body: JSON.stringify({
-      username: "a",
-      password: "a"
+export function loginWithToken(refreshToken, accessToken) {
+    console.log("Refresh token", refreshToken);
+    return fetch(serverPath + "/auth/login", {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            type: "token",
+            refreshtoken: refreshToken,
+            accesstoken: accessToken
+        },
+        body: JSON.stringify({
+            username: "a",
+            password: "a"
+        })
     })
-  })
-    .then(function(res) {
-      console.log("RRRRRRRRRRRRRRRRRRRRR", res);
-      return res.json();
-    })
-    .then(function(res) {
-      return new Promise((resolve, reject) => {
-        resolve(res);
-      });
-    })
-    .catch(err => {
-      console.log(err);
-    });
+        .then(function(res) {
+            console.log("RRRRRRRRRRRRRRRRRRRRR", res);
+            return res.json();
+        })
+        .then(function(res) {
+            return new Promise((resolve, reject) => {
+                resolve(res);
+            });
+        })
+        .catch(err => {
+            console.log(err);
+        });
 }
 export function signup(username, password) {
-  return fetch(serverPath + "/auth/register ", {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      username: username,
-      password: password
+    return fetch(serverPath + "/auth/register ", {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            username: username,
+            password: password
+        })
     })
-  })
-    .then(function(res) {
-      return res.json();
-    })
-    .then(function(res) {
-      return new Promise((resolve, reject) => {
-        resolve(res);
-        if (res.message === "ok") resolve(res);
-        else resolve(res);
-      });
-    })
-    .catch(err => {
-      console.log(err);
-    });
+        .then(function(res) {
+            return res.json();
+        })
+        .then(function(res) {
+            return new Promise((resolve, reject) => {
+                resolve(res);
+                if (res.message === "ok") resolve(res);
+                else resolve(res);
+            });
+        })
+        .catch(err => {
+            console.log(err);
+        });
 }
 export function getOldPosts(unique_id, location, lastpost) {
-  return fetch(serverPath + "/post", {
-    method: "GET",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-      unique_id: unique_id,
-      location: JSON.stringify(location),
-      lastpost: lastpost
-    }
-  })
-    .then(function(res) {
-      return res.json();
+    return fetch(serverPath + "/post", {
+        method: "GET",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            unique_id: unique_id,
+            location: JSON.stringify(location),
+            lastpost: lastpost
+        }
     })
-    .then(function(res) {
-      return new Promise((resolve, reject) => {
-        resolve(res.posts);
-      });
-    })
-    .catch(err => {
-      console.log(err);
-    });
+        .then(function(res) {
+            return res.json();
+        })
+        .then(function(res) {
+            return new Promise((resolve, reject) => {
+                resolve(res.posts);
+            });
+        })
+        .catch(err => {
+            console.log(err);
+        });
 }
 export function addPost(accessToken, refreshToken, location, text) {
-  return fetch(serverPath + "/post", {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-      accesstoken : accessToken,
-      refreshtoken : refreshToken,
-      location: JSON.stringify(location)
-    },
-    body: JSON.stringify({
-      text
+    return fetch(serverPath + "/post", {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            accesstoken: accessToken,
+            refreshtoken: refreshToken,
+            location: JSON.stringify(location)
+        },
+        body: JSON.stringify({
+            text
+        })
     })
-  })
-    .then(function(res) {
-      // console.log("Response" , res);
-      return;
-    })
-    .then(function(res) {
-      // func();
-      return;
-    })
-    .catch(err => {
-      console.log(err);
-    });
+        .then(function(res) {
+            // console.log("Response" , res);
+            return;
+        })
+        .then(function(res) {
+            // func();
+            return;
+        })
+        .catch(err => {
+            console.log(err);
+        });
 }
 export function likePost(accessToken, refreshToken, location, postId, like) {
-  var requestMethod;
-  like ? (requestMethod = "PUT") : (requestMethod = "DELETE");
-  return fetch(serverPath + "/post/" + postId + "/like", {
-    method: requestMethod,
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-      accesstoken : accessToken,
-      refreshtoken : refreshToken,
-      location: JSON.stringify(location)
-    }
-  })
-    .then(function(res) {
-      // console.log("Response" , res);
-      return;
+    var requestMethod;
+    like ? (requestMethod = "PUT") : (requestMethod = "DELETE");
+    return fetch(serverPath + "/post/" + postId + "/like", {
+        method: requestMethod,
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            accesstoken: accessToken,
+            refreshtoken: refreshToken,
+            location: JSON.stringify(location)
+        }
     })
-    .then(function(res) {
-      return;
-    })
-    .catch(err => {
-      console.log(err);
-    });
+        .then(function(res) {
+            // console.log("Response" , res);
+            return;
+        })
+        .then(function(res) {
+            return;
+        })
+        .catch(err => {
+            console.log(err);
+        });
 }
 export function getHotPosts(location, accessToken, refreshToken) {
-  console.log("parameters", location, accessToken, refreshToken);
-  return fetch(serverPath + "/post/hot", {
-    method: "GET",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-      location: JSON.stringify(location),
-      accesstoken: accessToken,
-      refreshtoken: refreshToken
-    }
-  })
-    .then(function(res) {
-      return res.json();
+    console.log("parameters", location, accessToken, refreshToken);
+    return fetch(serverPath + "/post/hot", {
+        method: "GET",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            location: JSON.stringify(location),
+            accesstoken: accessToken,
+            refreshtoken: refreshToken
+        }
     })
-    .then(function(res) {
-      return new Promise((resolve, reject) => {
-        resolve(res.posts);
-      });
-    })
-    .catch(err => {
-      console.log(err);
-    });
+        .then(function(res) {
+            return res.json();
+        })
+        .then(function(res) {
+            return new Promise((resolve, reject) => {
+                resolve(res.posts);
+            });
+        })
+        .catch(err => {
+            console.log(err);
+        });
 }
 export function getOldHotPosts(unique_id, location, lastpost) {
-  return fetch(serverPath + "/post/hot", {
-    method: "GET",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-      unique_id: unique_id,
-      location: JSON.stringify(location),
-      lastpost: lastpost
-    }
-  })
-    .then(function(res) {
-      return res.json();
+    return fetch(serverPath + "/post/hot", {
+        method: "GET",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            unique_id: unique_id,
+            location: JSON.stringify(location),
+            lastpost: lastpost
+        }
     })
-    .then(function(res) {
-      return new Promise((resolve, reject) => {
-        resolve(res.posts);
-      });
-    })
-    .catch(err => {
-      console.log(err);
-    });
+        .then(function(res) {
+            return res.json();
+        })
+        .then(function(res) {
+            return new Promise((resolve, reject) => {
+                resolve(res.posts);
+            });
+        })
+        .catch(err => {
+            console.log(err);
+        });
 }
