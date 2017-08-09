7ae93502e7c12e3642a632494b5ad57a220ac64c
sherafatish <sherafati.sh@gmail.com>
2017-08-09 09:49:04 +0430

    alert window added to logout bottom

--- main.js
+++ main.js
@@ -3,25 +3,26 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import {
-    TabNavigator,
-    StackNavigator,
-    NavigationActions
+  TabNavigator,
+  StackNavigator,
+  NavigationActions
 } from "react-navigation";
 import SendPostPage from "./SendPostPage";
 import {
-    AppRegistry,
-    StyleSheet,
-    Text,
-    View,
-    TextInput,
-    Button,
-    ScrollView,
-    TouchableOpacity,
-    Image,
-    RefreshControl,
-    FlatList,
-    ListView,
-    AsyncStorage
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  ScrollView,
+  TouchableOpacity,
+  Image,
+  RefreshControl,
+  FlatList,
+  ListView,
+  Alert,
+  AsyncStorage
 } from "react-native";
 import LoginScreen from "./screens/Login/login";
 import SignupScreen from "./screens/Signup/signup";
@@ -31,39 +32,36 @@ import HotPosts from "./HotPosts";
 import { logout } from "./actions";
 const store = createStore(reducer, applyMiddleware(thunk));
 const MainScreenNavigator = TabNavigator(
-    {
-        جدیدترین: { screen: NormalPosts },
-        داغ‌ترین: { screen: HotPosts }
-    },
-    {
-        tabBarOptions: {
-            style: {
-                backgroundColor: "#8BC34A"
-            }
-        }
+  {
+    جدیدترین: { screen: NormalPosts },
+    داغ‌ترین: { screen: HotPosts }
+  },
+  {
+    tabBarOptions: {
+      style: {
+        backgroundColor: "#8BC34A"
+      }
     }
+  }
 );
-const styles = StyleSheet.create({
-    logout: {
-        height: 35,
-        width: 35,
-        marginRight: 30
-    }
-});
 MainScreenNavigator.navigationOptions = props => {
-    return {
-        title: "اوناگی",
-        headerStyle: {
-            backgroundColor: "#8BC34A"
-        },
-        headerTitleStyle: {
-            color: "#fff",
-            fontFamily: "IRAN_Sans"
-        },
-        headerRight: (
-            <TouchableOpacity
-                onPress={() => {
-                    store.dispatch(logout());
+  return {
+    title: "اوناگی",
+    headerStyle: {
+      backgroundColor: "#8BC34A"
+    },
+    headerTitleStyle: {
+      color: "#fff",
+      fontFamily: "IRAN_Sans"
+    },
+    headerRight: (
+      <TouchableOpacity
+        onPress={() => {
+          Alert.alert(
+            'اوناگی',
+            'آیا قصد خروج از برنامه را دارید؟',
+            [
+              { text: 'بله', onPress: () => {store.dispatch(logout());
                     const resetAction = NavigationActions.reset({
                         index: 0,
                         actions: [
@@ -72,46 +70,51 @@ MainScreenNavigator.navigationOptions = props => {
                             })
                         ]
                     });
-                    props.navigation.dispatch(resetAction);
-                }}
-            >
-                <Image style={styles.logout} source={require("./logout.png")} />
-            </TouchableOpacity>
-        )
-    };
+                    props.navigation.dispatch(resetAction);} },
+              { text: 'خیر', onPress: () => console.log('Cancel Pressed') },
+              ],
+            { cancelable: false }
+          )
+        }}
+      >
+        <Image style={styleslogout={ height: 25, width: 25, marginRight: 10 }}
+         source={require("./logout.png")} />
+      </TouchableOpacity>
+    )
+  };
 };
 
 var auth = true;
 var App = null;
 const setup = () => {
-    class Root extends Component {
-        componentWillMount() {
-            var refreshToken = AsyncStorage.getItem(
-                "refreshToken",
-                (error, result) => {
-                    if (!result) auth = false;
-                    App = StackNavigator({
-                        Home: {
-                            screen: auth ? MainScreenNavigator : LoginScreen
-                        },
-                        SignUpPage: { screen: SignupScreen },
-                        MainScreen: { screen: MainScreenNavigator },
-                        SendPostPage: { screen: SendPostPage },
-                        LoginScreen: { screen: LoginScreen }
-                    });
-                    this.forceUpdate();
-                }
-            );
+  class Root extends Component {
+    componentWillMount() {
+      var refreshToken = AsyncStorage.getItem(
+        "refreshToken",
+        (error, result) => {
+          if (!result) auth = false;
+          App = StackNavigator({
+            Home: {
+                screen: auth ? MainScreenNavigator : LoginScreen
+            },
+            SignUpPage: { screen: SignupScreen },
+            MainScreen: { screen: MainScreenNavigator },
+            SendPostPage: { screen: SendPostPage },
+            LoginScreen: { screen: LoginScreen }
+          });
+          this.forceUpdate();
         }
-        render() {
-            if (!App) return null;
-            return (
-                <Provider store={store}>
-                    <App />
-                </Provider>
-            );
-        }
+      );
     }
-    return Root;
+    render() {
+      if (!App) return null;
+      return (
+        <Provider store={store}>
+          <App />
+        </Provider>
+      );
+    }
+  }
+  return Root;
 };
 AppRegistry.registerComponent("RahnemaTeam2App", setup);
