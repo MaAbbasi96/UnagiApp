2ef525f5a0f155838bb2e24ba525638076154010
sherafatish <sherafati.sh@gmail.com>
2017-08-09 10:10:57 +0430

    README file has been updated.

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # Rahnema-Team2-App
 
-npm install<br />
-npm install react-native-action-button<br />
-npm install react-native-vector-icons<br />
-npm i --save react-native fetch <br/>
-npm install react-navigation --save <br />
-react-native link (If you are facing with RNDeviceInfo error)
-npm i react-native-pull-to-refresh --save
+## About Project
+Unagi is a social media smart phone application.
+Presently it is available for Android.
+It allows people to create posts and view posts within a fixed radius around them.
+
+## Requirements
+Ensure that you have installed **absolutely everything** listed [here](https://facebook.github.io/react-native/docs/getting-started.html#content)
+
+## Installation
+npm install
--- main.js
+++ main.js
@@ -3,26 +3,26 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import {
-  TabNavigator,
-  StackNavigator,
-  NavigationActions
+    TabNavigator,
+    StackNavigator,
+    NavigationActions
 } from "react-navigation";
 import SendPostPage from "./SendPostPage";
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  TextInput,
-  Button,
-  ScrollView,
-  TouchableOpacity,
-  Image,
-  RefreshControl,
-  FlatList,
-  ListView,
-  Alert,
-  AsyncStorage
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    TextInput,
+    Button,
+    ScrollView,
+    TouchableOpacity,
+    Image,
+    RefreshControl,
+    FlatList,
+    ListView,
+    Alert,
+    AsyncStorage
 } from "react-native";
 import LoginScreen from "./screens/Login/login";
 import SignupScreen from "./screens/Signup/signup";
@@ -32,89 +32,107 @@ import HotPosts from "./HotPosts";
 import { logout } from "./actions";
 const store = createStore(reducer, applyMiddleware(thunk));
 const MainScreenNavigator = TabNavigator(
-  {
-    جدیدترین: { screen: NormalPosts },
-    داغ‌ترین: { screen: HotPosts }
-  },
-  {
-    tabBarOptions: {
-      style: {
-        backgroundColor: "#8BC34A"
-      }
+    {
+        جدیدترین: { screen: NormalPosts },
+        داغ‌ترین: { screen: HotPosts }
+    },
+    {
+        tabBarOptions: {
+            style: {
+                backgroundColor: "#8BC34A"
+            }
+        }
     }
-  }
 );
 MainScreenNavigator.navigationOptions = props => {
-  return {
-    title: "اوناگی",
-    headerStyle: {
-      backgroundColor: "#8BC34A"
-    },
-    headerTitleStyle: {
-      color: "#fff",
-      fontFamily: "IRAN_Sans"
-    },
-    headerRight: (
-      <TouchableOpacity
-        onPress={() => {
-          Alert.alert(
-            'اوناگی',
-            'آیا قصد خروج از برنامه را دارید؟',
-            [
-              { text: 'بله', onPress: () => {store.dispatch(logout());
-                    const resetAction = NavigationActions.reset({
-                        index: 0,
-                        actions: [
-                            NavigationActions.navigate({
-                                routeName: "LoginScreen"
-                            })
-                        ]
-                    });
-                    props.navigation.dispatch(resetAction);} },
-              { text: 'خیر', onPress: () => console.log('Cancel Pressed') },
-              ],
-            { cancelable: false }
-          )
-        }}
-      >
-        <Image style={styleslogout={ height: 25, width: 25, marginRight: 10 }}
-         source={require("./logout.png")} />
-      </TouchableOpacity>
-    )
-  };
+    return {
+        title: "اوناگی",
+        headerStyle: {
+            backgroundColor: "#8BC34A"
+        },
+        headerTitleStyle: {
+            color: "#fff",
+            fontFamily: "IRAN_Sans"
+        },
+        headerRight: (
+            <TouchableOpacity
+                onPress={() => {
+                    Alert.alert(
+                        "اوناگی",
+                        "آیا قصد خروج از برنامه را دارید؟",
+                        [
+                            {
+                                text: "بله",
+                                onPress: () => {
+                                    store.dispatch(logout());
+                                    const resetAction = NavigationActions.reset(
+                                        {
+                                            index: 0,
+                                            actions: [
+                                                NavigationActions.navigate({
+                                                    routeName: "LoginScreen"
+                                                })
+                                            ]
+                                        }
+                                    );
+                                    props.navigation.dispatch(resetAction);
+                                }
+                            },
+                            {
+                                text: "خیر",
+                                onPress: () => console.log("Cancel Pressed")
+                            }
+                        ],
+                        { cancelable: false }
+                    );
+                }}
+            >
+                <Image
+                    style={
+                        (styleslogout = {
+                            height: 25,
+                            width: 25,
+                            marginRight: 10
+                        })
+                    }
+                    source={require("./logout.png")}
+                />
+            </TouchableOpacity>
+        )
+    };
 };
 
 var auth = true;
 var App = null;
 const setup = () => {
-  class Root extends Component {
-    componentWillMount() {
-      var refreshToken = AsyncStorage.getItem(
-        "refreshToken",
-        (error, result) => {
-          if (!result) auth = false;
-          App = StackNavigator({
-            Home: {
-                screen: auth ? MainScreenNavigator : LoginScreen
-            },
-            SignUpPage: { screen: SignupScreen },
-            MainScreen: { screen: MainScreenNavigator },
-            SendPostPage: { screen: SendPostPage },
-            LoginScreen: { screen: LoginScreen }
-          });
-          this.forceUpdate();
+    class Root extends Component {
+        componentWillMount() {
+            var refreshToken = AsyncStorage.getItem(
+                "refreshToken",
+                (error, result) => {
+                    if (!result) auth = false;
+                    App = StackNavigator({
+                        Home: {
+                            screen: auth ? MainScreenNavigator : LoginScreen
+                        },
+                        SignUpPage: { screen: SignupScreen },
+                        MainScreen: { screen: MainScreenNavigator },
+                        SendPostPage: { screen: SendPostPage },
+                        LoginScreen: { screen: LoginScreen }
+                    });
+                    this.forceUpdate();
+                }
+            );
         }
-      );
+        render() {
+            if (!App) return null;
+            return (
+                <Provider store={store}>
+                    <App />
+                </Provider>
+            );
+        }
     }
-    render() {
-      if (!App) return null;
-      return (
-        <Provider store={store}>
-          <App />
-        </Provider>
-      );
-    }
-  }
-  return Root;
+    return Root;
 };
 AppRegistry.registerComponent("RahnemaTeam2App", setup);
