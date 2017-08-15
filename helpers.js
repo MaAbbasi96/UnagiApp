da97b028c9a054306f5549f08c2cf966d757f129
Amirali Esmaeili <esmailiamirali@gmail.com>
2017-08-15 14:45:09 +0430

    Implemented regex validations in helpers

--- helpers.js
+++ helpers.js
@@ -51,3 +51,19 @@ export function RandomColor() {
     let color = Colors[parseInt(Math.random() * Colors.length)];
     return color;
 }
+
+//Regex Patterns
+const UserNamePatt = /^[A-Za-z0-9_.]{6,16}$/g;
+const PassWordPatt = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d$@$!%*?&]{6,32}$/g;
+const EmailPatt = /^[a-z0-9]+([-+._][a-z0-9]+){0,2}@.*?(\.(a(?:[cdefgilmnoqrstuwxz]|ero|(?:rp|si)a)|b(?:[abdefghijmnorstvwyz]iz)|c(?:[acdfghiklmnoruvxyz]|at|o(?:m|op))|d[ejkmoz]|e(?:[ceghrstu]|du)|f[ijkmor]|g(?:[abdefghilmnpqrstuwy]|ov)|h[kmnrtu]|i(?:[delmnoqrst]|n(?:fo|t))|j(?:[emop]|obs)|k[eghimnprwyz]|l[abcikrstuvy]|m(?:[acdeghklmnopqrstuvwxyz]|il|obi|useum)|n(?:[acefgilopruz]|ame|et)|o(?:m|rg)|p(?:[aefghklmnrstwy]|ro)|qa|r[eosuw]|s[abcdeghijklmnortuvyz]|t(?:[cdfghjklmnoprtvwz]|(?:rav)?el)|u[agkmsyz]|v[aceginu]|w[fs]|y[etu]|z[amw])\b){1,2}$/g;
+
+//Regex Checkers
+export function ValidateUsername(usernameStr) {
+    return UserNamePatt.test(usernameStr);
+}
+export function ValidatePassword(passwordStr) {
+    return PassWordPatt.test(passwordStr);
+}
+export function ValidateEmail(emailStr) {
+    return EmailPatt.test(emailStr);
+}
